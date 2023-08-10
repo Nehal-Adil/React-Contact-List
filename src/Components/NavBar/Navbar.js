f7ca55a15785f7221fdc2b0650744436e4c0a38f
Nehal <mdnehaladil@gmail.com>
2023-08-10 16:56:19 +0530

    add Navbar

---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+import Style from "./Navbar.module.css";
+
+function NavBar() {
+  return (
+    <>
+      <nav>
+        <Link className="link" to="/">
+          <h2 className={Style.heading}>Contact List App</h2>
+        </Link>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+}
+
+export default NavBar;
